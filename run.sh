fc327a8568971879a47c1bb0d5e2979504a3ee14
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2025-05-21 10:15:14 -0700

    Added absolute path for streamlit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python -m venv financial-agent && source financial-agent/bin/activate && pip install -r requirements.txt
 source /workspaces/$RepositoryName/setEnv.sh
-streamlit run main.py
+/workspaces/$RepositoryName/financial-agent/bin/streamlit run main.py
